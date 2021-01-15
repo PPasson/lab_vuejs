e1a8430de643ede64e47f2761c73e2b13ef50d69
PPasson <passonpp@gmail.com>
2021-01-15 17:05:20 +0700

    update demo9: v-for vs v-model to dynamic Add and remove

--- src/views/Demo9_Loop.vue
+++ src/views/Demo9_Loop.vue
@@ -22,14 +22,37 @@
         {{ title }}, Price {{ price }}
       </li>
     </ul>
+
+    <input type="text" placeholder="Title" v-model="course.title" /><br />
+    <input type="number" placeholder="Price" v-model="course.price" /><br />
+    <button @click="addCourse">Submit</button>
+
+    <!--Dynamic-->
+    <ul>
+      <li v-for="(course, index) in dynamicCourse" :key="index">
+        {{ course.title }}, Price {{ course.price }} <button @click="removeCourse(index)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "Demo9",
+  methods: {
+      addCourse(){
+          this.dynamicCourse.push(this.course)
+          this.course = {title: "", price: 0}
+
+      },
+      removeCourse(index){
+          this.dynamicCourse.splice(index, 1)
+      }
+  },
   data() {
     return {
+      course: { title: "", price: 0 },
+      dynamicCourse: [],
       mCourse: [
         { title: "iOS", price: 11 },
         { title: "Android", price: 12 },
