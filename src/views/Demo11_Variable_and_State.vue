1a1f9b9cb7450b3ed215ddf68c08a32b5ac7b738
PPasson <passonpp@gmail.com>
2021-01-16 14:36:34 +0700

    add Demo11: Normal vs State Variables

--- src/views/Demo11_Variable_and_State.vue
+++ src/views/Demo11_Variable_and_State.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="Demo11">
-    {{count}}
+    <div>{{ count }}</div>
+    <div>{{ Math.random() }}</div>
+    <div>Timestamp : {{ timestamp }}</div>
+    <button @click="timestamp = Date.now()">Update Timestamp</button>
+    <button @click="count++">Update Count</button>
   </div>
 </template>
 
 <script>
 export default {
-    name: "demo11",
-    data(){
-        count: 0
-    }
-}
+  name: "demo11",
+  created(){
+    this.timestamp = 0  
+  },
+  mounted() {
+    setInterval(() => {
+      //this.count++;
+      console.log("Timestamp: " + this.timestamp);
+    }, 100);
+  },
+  data() {
+    return {
+      count: 0 //local state
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
